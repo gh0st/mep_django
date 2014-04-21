6a42b242a19787939f94562960cd26da7d25ab7e
danthemanvsqz <danthemanvsqz@gmail.com>
2014-04-21 00:14:59 -0700

    cleaned up some unneeded templates

--- mep_django/urls.py
+++ mep_django/urls.py
@@ -10,11 +10,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^$', 'mep_django.linkedin.views.home'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^signup-email/', 'mep_django.linkedin.views.signup_email'),
-    url(r'^email-sent/', 'mep_django.linkedin.views.validation_sent'),
     url(r'^login/$', 'mep_django.linkedin.views.home'),
     url(r'^logout/$', 'mep_django.linkedin.views.logout'),
     url(r'^done/$', 'mep_django.linkedin.views.done', name='done'),
-    url(r'^email/$', 'mep_django.linkedin.views.require_email', name='require_email'),
     url('', include('social.apps.django_app.urls', namespace='social')),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
