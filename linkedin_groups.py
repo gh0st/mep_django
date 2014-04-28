9c6b3ab3d6513fabfc238c14d05cfd0a6cf27c6d
Dan Vasquez <danthemanvsqz@gmail.com>
2014-04-28 15:33:59 -0700

    building server script, added models for linkedin groups

--- linkedin_groups.py
+++ linkedin_groups.py
@@ -1,5 +1,20 @@
 from linkedin import linkedin
+
 import pprint
+
+from django.conf import settings
+settings.configure(
+    DATABASE_ENGINE = 'django.db.backends.sqlite3',
+    DATABASE_NAME = 'db.sqlite3',
+    DATABASE_USER = 'danthemanvsqz',
+    DATABASE_PASSWORD = 'Aa1234',
+    #DATABASE_HOST = 'localhost',
+    #DATABASE_PORT = '5432',
+    #TIME_ZONE = 'America/New_York',
+)
+
+from mep_django.linkedin.models import GroupPost
+
 
 USER_TOKEN = '0cf4b73d-ed4c-474f-b62c-620404f94546'
 USER_TOKEN_SECRET = '7dc3d37b-a45c-44f6-9e04-fa2e645cade9'
@@ -13,9 +28,10 @@ auth = linkedin.LinkedInDeveloperAuthentication(DEV_KEY, DEV_SECRET, USER_TOKEN,
 
 app = linkedin.LinkedInApplication(auth)
 
-GROUP_ID = 1917190
+#GROUP_ID = 1917190
+GROUP_ID = 6683349
 
-posts = app.get_posts(GROUP_ID, selectors=['title', 'summary', 'creation-timestamp', 'attachment', 'site-group-post-url',], params={'count':10})
+posts = app.get_posts(GROUP_ID, selectors=['title', 'summary', 'creation-timestamp', 'site-group-post-url',], params={'count':10})
 print type(posts)
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(posts)
--- mep_django/linkedin/models.py
+++ mep_django/linkedin/models.py
@@ -7,3 +7,12 @@ from django.db import models
 class CustomUser(AbstractUser):
     objects = UserManager()
 '''
+
+
+class GroupPost(models.Model):
+    title = models.CharField(max_length = 500)
+    summary = models.TextField()
+    creation_timestamp = models.BigIntegerField()
+    db_timestamp = models.DateField(auto_now_add = True)
+    site_group_post_url = models.URLField()
+    
